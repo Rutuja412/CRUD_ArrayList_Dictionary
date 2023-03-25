bf56e34425f9239d084e9b2574b6253b01015274
Rutuja412 <rutujakadam4545@gmail.com>
2023-03-25 22:39:31 +0530

    [Rutuja]:Welcome

---
+++ ArrayList_Dictionary_NLog/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArrayList_Dictionary_NLog
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WelCome to Arraylist ,Dictionary Demo");
+        }
+    }
+}
